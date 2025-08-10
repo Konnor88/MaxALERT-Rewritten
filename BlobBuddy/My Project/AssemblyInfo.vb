729f3535cc62532e696ba2db772c89d48cec98a2
Konnor88 <39319697+Konnor88@users.noreply.github.com>
2025-08-10 10:55:56 -0400

    Update AssemblyInfo.vb

--- BlobBuddy/My Project/AssemblyInfo.vb
+++ BlobBuddy/My Project/AssemblyInfo.vb
@@ -33,7 +33,8 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.139.154")> 
-<Assembly: AssemblyFileVersion("1.0.139.154")> 
+<Assembly: AssemblyVersion("1.1.139.154")> 
+<Assembly: AssemblyFileVersion("1.1.139.154")> 
+
 
 <Assembly: NeutralResourcesLanguageAttribute("en-US")> 
